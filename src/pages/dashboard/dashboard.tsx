c9161beb0e84c06cfa8592396b47c55d56f5da84
algafur <algafura@gmail.com>
2024-05-15 00:24:31 +0500

    fix: payment filters

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,14 +1,41 @@
-import { Table } from 'antd'
+import { Select, Table } from 'antd'
 import { usePaymentData } from 'entities/dashboard/payment-data'
 import { useTableData } from 'entities/dashboard/table-data'
-import { FC } from 'react'
+import React, { FC } from 'react'
 
 export const Dashboard: FC = () => {
-  const { fetchData, columns } = usePaymentData()
+  const { fetchData, columns, postSelect } = usePaymentData()
   const { paymentTable, paymentTableSum, totalPaymentTable } = useTableData()
 
   return (
     <>
+      <div style={{ marginBottom: '10px', display: 'flex', gap: '10px' }}>
+        <div>
+          <div style={{ marginBottom: '5px' }}>Посты</div>
+          <Select
+            mode={'multiple'}
+            allowClear={true}
+            options={postSelect}
+            maxTagCount={1}
+            onChange={(value) => {
+              fetchData()
+            }}
+            style={{ width: 200 }}
+          />
+        </div>
+
+        <div>
+          <div style={{ marginBottom: '5px' }}>Дата</div>
+          <Select
+            options={[]}
+            style={{ width: 200 }}
+            onChange={(value) => {
+              fetchData()
+            }}
+          />
+        </div>
+      </div>
+
       <Table
         columns={columns}
         dataSource={paymentTable}
